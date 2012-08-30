b072956cfb68fad865d5804602b8957e60a7c4a5
Manuel Odendahl <wesen@ruinwesen.com>
2012-08-30 15:11:38 +0200

    bump

--- index.php
+++ index.php
@@ -57,7 +57,7 @@ function clean_output(&$output){
 
 $chart_data = $_REQUEST['chart'];
 $output_format = (isset($_REQUEST['format'])) ? strtolower($_REQUEST['format']) : "svg";
-$convert_args = $_REQUEST['convert_args'];
+$convert_args = (isset($_REQUEST['convert_args']) ? $_REQUEST['convert_args'] : "");
 
 // sample chart for testing
 //$chart_data = '{"chart":{"defaultSeriesType":"area","renderTo":"containerfoo","renderer":"SVG"},"title":{"text":"Sales By Partner"},"subtitle":{"text":"Source: SecurityTrax"},"xAxis":{"categories":["11\/01\/2011","11\/02\/2011","11\/03\/2011","11\/04\/2011","11\/05\/2011","11\/06\/2011","11\/07\/2011","11\/08\/2011","11\/09\/2011","11\/10\/2011","11\/11\/2011","11\/12\/2011","11\/13\/2011","11\/14\/2011","11\/15\/2011","11\/16\/2011","11\/17\/2011","11\/18\/2011","11\/19\/2011","11\/20\/2011","11\/21\/2011","11\/22\/2011","11\/23\/2011","11\/24\/2011","11\/25\/2011","11\/26\/2011","11\/27\/2011","11\/28\/2011","11\/29\/2011","11\/30\/2011"],"tickmarkPlacement":"on"},"yAxis":{"title":{"text":"Sales"}},"plotOptions":{"area":{"stacking":"normal","lineWidth":1,"lineColor":"#666666","marker":{"lineWidth":1,"lineColor":"#666666"}}},"series":[{"name":"HiValley","data":[0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],"animation":false}]}';
@@ -109,6 +109,7 @@ if( ! empty($theme)){
     }
 }
 
+$js_content = "";
 foreach($included_javascript as $path){
     $js_content .= "\n" . trim(file_get_contents($path)) . "\n";
 }
@@ -146,7 +147,6 @@ file_put_contents($tmp_file_path, $html);
 
 $command = $phantomjs_path . " $phantomjs_cmd_extra_options " . $phantomjs_script . " " . $tmp_file_path;
 $svg = shell_exec(escapeshellcmd($command));
-
 clean_output($svg);
 
 // testing output format
@@ -157,9 +157,10 @@ if( ! empty($svg)){
         case "jpg":
         case "jpeg":
         case "png":
-            $img = shell_exec("echo " . escapeshellarg($svg) . " | convert -background transparent $convert_args svg:- $output_format:-");
-            header("Content-Type: image/$output_format");
-            echo $img;
+      $cmd = "echo " . escapeshellarg($svg) . " | /usr/local/bin/convert -background transparent $convert_args svg:- $output_format:-";
+      $img = shell_exec($cmd);
+      header("Content-Type: image/$output_format");
+      echo $img;
             break;
         case "svg":
         default:
